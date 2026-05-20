00320d25a1066a2c0628edbaa22edf54e2e6dc25
abdiaziz <abdiazizabdullahi40@gmail.com>
2026-05-21 00:32:52 +0300

    MA-58 Creation of context

---
+++ Frontend/src/context/AuthContext.jsx
@@ -0,0 +1,52 @@
+import React, { createContext, useState, useEffect, useContext } from "react";
+
+const AuthContext = createContext(null);
+
+export const AuthProvider = ({ children }) => {
+  const [token, setToken] = useState(localStorage.getItem("token"));
+  const [user, setUser] = useState(JSON.parse(localStorage.getItem("user")));
+
+  const login = (accessToken, userData) => {
+    localStorage.setItem("token", accessToken);
+    localStorage.setItem("user", JSON.stringify(userData));
+    setToken(accessToken);
+    setUser(userData);
+  };
+
+  const logout = () => {
+    localStorage.removeItem("token");
+    localStorage.removeItem("user");
+    setToken(null);
+    setUser(null);
+  };
+
+  const fetchWithAuth = async (url, options = {}) => {
+    const headers = {
+      "Content-Type": "application/json",
+      ...options.headers,
+    };
+
+    if (token) {
+      headers["Authorization"] = `Bearer ${token}`;
+    }
+
+    const response = await fetch(`http://localhost:5001${url}`, {
+      ...options,
+      headers,
+    });
+    if (response.status === 401) {
+      logout();
+    }
+    return response;
+  };
+
+  return (
+    <AuthContext.Provider
+      value={{ token, user, login, logout, fetchWithAuth, setUser }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export const useAuth = () => useContext(AuthContext);
